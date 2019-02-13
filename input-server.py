736d0afd2694472dd9def57707b5e6f446c6fcd0
Erik Liodden <eriklio@stud.ntnu.no>
2019-02-13 15:49:03 +0100

    add simple udp-server
    simple udp-server written in python to fetch information relayed from
    telenor. Currently it just append the payload to a file, 'out2.txt'.

---
+++ input-server.py
@@ -0,0 +1,16 @@
+import socket
+import datetime
+
+port = 9001
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Accept UDP datagrams, on the given port, from any sender
+s.bind(("", port))
+print("waiting on port: ", port)
+while True:
+    # Receive up to 1,024 bytes in a datagram
+    data, addr = s.recvfrom(1024)
+    now = datetime.datetime.now()
+    with open("out2.txt", "a") as f:
+        f.writelines("{}: {}\n".format(now, data.decode("utf-8")))
+    print("Received:", data, " from ", addr)
