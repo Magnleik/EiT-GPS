d8946f34fe029d847b0becf746c620646d24b332
stianbm <33124209+stianbm@users.noreply.github.com>
2019-02-27 14:30:55 +0100

    Get request now works

--- js/react-app/src/App.js
+++ js/react-app/src/App.js
@@ -1,41 +1,64 @@
 import React, { Component } from "react";
 import "./App.css";
 import Map from "./components/MapComponent";
-import Sidebar from "./components/SidebarComponent"
-
+import Sidebar from "./components/SidebarComponent";
 
 const apiurl = "http://eit19gps-api-heroku.herokuapp.com/request";
-const deviceID = "id1"
+const deviceID = "17dh0cf43jfkmm";
 
 class App extends Component {
-
-  state = {"data": [
-    {"time": 1550665908, "lat": 63.415, "lng": 10.3},
-    {"time": 1550666908, "lat": 63.310, "lng": 10.5},
-    {"time": 1550667908, "lat": 63.123456, "lng": 10.4},
-    {"time": 1550668908, "lat": 63.09876, "lng": 10.12345}
-  ]};
+  state = {
+    data: [
+      { time: 1550665908, lat: 63.415, lng: 10.3 },
+      { time: 1550666908, lat: 63.31, lng: 10.5 },
+      { time: 1550667908, lat: 63.123456, lng: 10.4 },
+      { time: 1550668908, lat: 63.09876, lng: 10.12345 }
+    ]
+  };
 
   componentDidMount() {
-    fetch(apiurl + "?id=" + deviceID, {mode: 'cors', headers: {Accept: 'application/json', 'Access-Control-Allow-Origin': '*'}})
+    /*
+    fetch(apiurl + "?id=" + deviceID, {
+      mode: "cors",
+      headers: {
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*"
+      }
+    })
       .then(res => {
-        console.log(res)
-        res.json(); 
+        console.log(res);
+        res.json();
       })
       .then(data => {
-        this.setState({data: data});
+        this.setState({ data: data });
         // console.log(data);
-      })
+      });
+      */
+    request();
   }
 
   render() {
     return (
       <div className="rows">
-        <Sidebar height={window.innerHeight + "px"} width={window.innerWidth*0.2 + "px"} data={this.state.data}/>
-        <Map height={window.innerHeight + "px"} width={window.innerWidth*0.8 + "px"} data={this.state.data}/>
+        <Sidebar
+          height={window.innerHeight + "px"}
+          width={window.innerWidth * 0.2 + "px"}
+          data={this.state.data}
+        />
+        <Map
+          height={window.innerHeight + "px"}
+          width={window.innerWidth * 0.8 + "px"}
+          data={this.state.data}
+        />
       </div>
     );
   }
 }
+
+const request = async () => {
+  const response = await fetch(apiurl + "?id=" + deviceID);
+  const json = await response.json();
+  console.log(json);
+};
 
 export default App;
